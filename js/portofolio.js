1d826a4750968e4fe95d6bf09c0f8d85af1d7bb8
Huracan <aapcx55@gmail.com>
2016-11-07 14:34:23 -0500

    cambios

--- js/portofolio.js
+++ js/portofolio.js
@@ -1,33 +1,26 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+$(document).ready(function(){
 
-// jQuery to collapse the navbar on scroll
-function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-}
+  // Write the brand text with typed.js
+  $(function(){
+      $(".tpd1").typed({
+        strings: ["Ingeniero en sistemas de información."],
+        typeSpeed: 10,
+        startDelay:500,
+        showCursor: false,
+        callback: function(){
+          $(".tpd2").typed({
+            strings: ["Desarrollador web desde 2008."],
+            typeSpeed: 10,
+            startDelay:500,
+            showCursor: false,
+          });
+        }
+      });
+  });
 
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+      $(this).closest('.collapse').collapse('toggle');
+  });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-/*$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});*/
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
 });
